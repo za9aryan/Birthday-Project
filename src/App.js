b8d7de6c91467e663a55cf80f3f2a2da3a331f1f
za9aryan <71818498+za9aryan@users.noreply.github.com>
2021-06-30 18:30:28 +0400

    hj

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h2>reminder project setup</h2>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPeople([]);
+          }}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
